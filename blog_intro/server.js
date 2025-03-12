62a71f74aede2c55a04d43ebe255f31fc9883550
Willians-Hurtado <willianshurtado5@gmail.com>
2025-03-12 15:37:56 +0100

    add array with posts of my blog

---
+++ blog_intro/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express()
+const port = 3001
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
+
+app.get('/', (req, res) => {
+    console.log(res);
+    
+    res.send(`Server del mio blog`)
+})
+
+app.get('/api/v1/dolci', (req, res) => {
+    const dolci = [
+        {
+            Title: 'Ciambellone',
+            img: '/asset/img/ciambellone.jpeg',
+            Tags: ["ciambellone", "dolci", "colazione", "merenda", "soffice"],
+                
+        },
+        {
+            Title: 'Cracker_barbabietola',
+            img: '/asset/img/cracker_barbabietola.jpeg',
+            Tags: ["cracker", "dolci", "colazione", "merenda", "soffice"],
+                
+        },
+        {
+            Title: 'Pane fritto dolce',
+            img: '/asset/img/pane_fritto_dolce.jpeg',
+            Tags: ["Pane fritto dolce", "dolci", "colazione", "merenda", "soffice"],
+                
+        },
+        {
+            Title: 'Pasta barbabietola',
+            img: '/asset/img/pasta_barbabietola.jpeg',
+            Tags: ["Pasta barbabietola", "dolci", "colazione", "merenda", "soffice"],
+                
+        },
+        {
+            Title: 'Torta paesana',
+            img: '/asset/img/torta_paesana.jpeg',
+            Tags: ["Torta paesana", "dolci", "colazione", "merenda", "soffice"],
+                
+        },
+            
+    ]
+    
+        
+    console.log(dolci);
+    res.json(dolci);
+        
+})
+
+    
+    
+    
+
